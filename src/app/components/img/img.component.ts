9cad96751f7616055a6f1a02a0317c66d75479d0
AdrianGaray <adrgaray@emeal.nttdata.com>
2023-04-05 18:06:16 -0300

    Se agrega el componente Header

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,6 +1,6 @@
 // agregar la detencion de cambios: OnChange
 // agregar cuando se le elimina el componente OnDestroy
-import { Component, Input, Output, EventEmitter, OnInit ,OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit ,OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -10,7 +10,17 @@ import { Component, Input, Output, EventEmitter, OnInit ,OnChanges, AfterViewIni
 
 // implementar la interfaz: OnInit y OnChanges
 export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
-  @Input() img: string = '';
+  img: string = '';
+
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img  =>' ,this.img);
+    // code
+  }
+
+  @Input() alt: string = '';
+
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
 
@@ -25,12 +35,13 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
     console.log('constructor', 'imgValue =>', this.img);
   }
 
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges) {
     // before -- during render -- corre antes del render y dureante, porq va estar inpeccionando cambios
     // changes inputs -- multiples times -- su objetivo como tal es actualizando los cambios en los INPUTS. Coore muchas veces, las veces que actualizamos los inputs de nuestro componentes
     // por ejemplo, cada vez desde el padre actulizamos el valor img recibimos esa actualizacion aca
     // angular tiene que montar el componente <app-img></app-img> (alistarlo - prepararlo)  y todos esos eventos van a correr antes del render
     console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log('changes', changes);
   }
 
   ngOnInit(): void {
