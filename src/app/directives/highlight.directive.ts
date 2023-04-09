310835fbcc9e36c48e673a2d20447bf70232a3e4
AdrianGaray <adrgaray@emeal.nttdata.com>
2023-04-09 17:07:04 -0300

    Creacion de directivas

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,23 @@
+// importacion de ElementRef
+// importacion HostListener
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.element.nativeElement.style.backgroundColor = 'red';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.element.nativeElement.style.backgroundColor = '';
+  }
+
+  // hacer inyeccion de idenpendencia de ElementRef
+  constructor(private element: ElementRef) {
+    // this.element.nativeElement.style.backgroundColor = 'red';
+  }
+
+}
